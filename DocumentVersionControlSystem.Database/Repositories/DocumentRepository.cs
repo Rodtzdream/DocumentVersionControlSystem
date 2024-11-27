b34993b7ecf16a3433dfdd93847090e2c92010ee
Yurii Oliinyk <olijnikura@gmail.com>
2024-11-28 00:02:33 +0200

    Added Services (Repositories) for Document and Version

--- DocumentVersionControlSystem.Database/Repositories/DocumentRepository.cs
+++ DocumentVersionControlSystem.Database/Repositories/DocumentRepository.cs
@@ -1,6 +1,46 @@
-﻿namespace DocumentVersionControlSystem.Database.Repositories
+﻿namespace DocumentVersionControlSystem.Database.Repositories;
+
+public class DocumentRepository
 {
-    internal class DatabaseContext
+    private readonly DatabaseContext _context;
+
+    public DocumentRepository(DatabaseContext context)
     {
+        _context = context;
+    }
+
+    public void AddDocument(DocumentVersionControlSystem.Database.Models.Document document)
+    {
+        _context.Documents.Add(document);
+    }
+
+    public DocumentVersionControlSystem.Database.Models.Document GetDocumentById(int id)
+    {
+        return _context.Documents.Find(id);
+    }
+
+    public List<DocumentVersionControlSystem.Database.Models.Document> GetAllDocuments()
+    {
+        return _context.Documents.ToList();
+    }
+
+    public void UpdateDocument(DocumentVersionControlSystem.Database.Models.Document document)
+    {
+        _context.Documents.Update(document);
+    }
+
+    public void DeleteDocument(DocumentVersionControlSystem.Database.Models.Document document)
+    {
+        _context.Documents.Remove(document);
+    }
+
+    public List<DocumentVersionControlSystem.Database.Models.Document> GetDocumentsByName(string name)
+    {
+        return _context.Documents.Where(d => d.Name == name).ToList();
+    }
+
+    public void SaveChanges()
+    {
+        _context.SaveChanges();
     }
 }
--- DocumentVersionControlSystem.Database/Repositories/VersionRepository.cs
+++ DocumentVersionControlSystem.Database/Repositories/VersionRepository.cs
@@ -1,6 +1,41 @@
-﻿namespace DocumentVersionControlSystem.Database.Repositories
+﻿namespace DocumentVersionControlSystem.Database.Repositories;
+
+public class VersionRepository
 {
-    internal class VersionRepository
+    private readonly DatabaseContext _context;
+
+    public VersionRepository(DatabaseContext context)
     {
+        _context = context;
+    }
+
+    public void AddVersion(DocumentVersionControlSystem.Database.Models.Version version)
+    {
+        _context.Versions.Add(version);
+    }
+
+    public DocumentVersionControlSystem.Database.Models.Version GetVersionById(int id)
+    {
+        return _context.Versions.Find(id);
+    }
+
+    public List<DocumentVersionControlSystem.Database.Models.Version> GetAllVersions()
+    {
+        return _context.Versions.ToList();
+    }
+
+    public void DeleteVersion(DocumentVersionControlSystem.Database.Models.Version version)
+    {
+        _context.Versions.Remove(version);
+    }
+
+    public List<DocumentVersionControlSystem.Database.Models.Version> GetVersionsByDocumentId(int documentId)
+    {
+        return _context.Versions.Where(v => v.DocumentId == documentId).ToList();
+    }
+
+    public void SaveChanges()
+    {
+        _context.SaveChanges();
     }
 }
