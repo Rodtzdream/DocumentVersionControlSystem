613891c5ccdcdf16ff7b3476c84ba4909a9b085b
Yurii Oliinyk <olijnikura@gmail.com>
2024-11-28 13:26:55 +0200

    Implemented FileStorageManager

--- DocumentVersionControlSystem.FileStorage/FileStorageManager.cs
+++ DocumentVersionControlSystem.FileStorage/FileStorageManager.cs
@@ -1,7 +1,29 @@
-﻿namespace DocumentVersionControlSystem.FileStorage
-{
-    public class FileStorageManager
-    {
+﻿namespace DocumentVersionControlSystem.FileStorage;
 
+public class FileStorageManager
+{
+    public void SaveFile(string FilePath, string content)
+    {
+        System.IO.File.WriteAllText(FilePath, content);
+    }
+
+    public string ReadFile(string FilePath)
+    {
+        return System.IO.File.ReadAllText(FilePath);
+    }
+
+    public void DeleteFile(string FilePath)
+    {
+        System.IO.File.Delete(FilePath);
+    }
+
+    public bool FileExists(string FilePath)
+    {
+        return System.IO.File.Exists(FilePath);
+    }
+
+    public void CopyFile(string sourceFilePath, string destinationFilePath)
+    {
+        System.IO.File.Copy(sourceFilePath, destinationFilePath);
     }
 }
