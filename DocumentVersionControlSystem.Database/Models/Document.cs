5ada17feae108780b1e9946d30373c0630d746d8
Yurii Oliinyk <olijnikura@gmail.com>
2024-12-18 02:11:50 +0200

    Refactor Document.cs for cleaner namespace and class format

--- DocumentVersionControlSystem.Database/Models/Document.cs
+++ DocumentVersionControlSystem.Database/Models/Document.cs
@@ -1,12 +1,11 @@
-﻿namespace DocumentVersionControlSystem.Database.Models
+﻿namespace DocumentVersionControlSystem.Database.Models;
+
+public class Document
 {
-    public class Document
-    {
-        public int Id { get; set; }
-        public required string Name { get; set; }
-        public DateTime CreationDate { get; set; }
-        public DateTime LastModifiedDate { get; set; }
-        public required string FilePath { get; set; }
-        public int VersionCount { get; set; }
-    }
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public DateTime CreationDate { get; set; }
+    public DateTime LastModifiedDate { get; set; }
+    public required string FilePath { get; set; }
+    public int VersionCount { get; set; }
 }
