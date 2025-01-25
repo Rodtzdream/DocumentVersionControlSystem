61ce4a529e51c5ff97ecf102bc0a4c970b6af8cc
Yurii Oliinyk <olijnikura@gmail.com>
2025-01-25 23:02:36 +0200

    Add switch option handling and message text initialization and Refactor InputPopup and SelectSwitchOptionPopup classes

--- DocumentVersionControlSystem.UI/Popups/InputPopup.xaml.cs
+++ DocumentVersionControlSystem.UI/Popups/InputPopup.xaml.cs
@@ -1,40 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
+namespace DocumentVersionControlSystem.UI.Popups;
 
-namespace DocumentVersionControlSystem.UI.Popups
+
+public partial class InputPopup : Window
 {
-    /// <summary>
-    /// Interaction logic for 
-    /// .xaml
-    /// </summary>
-    public partial class InputPopup : Window
+    public InputPopup(string _messageText = "")
     {
-        public InputPopup()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
+        this.MessageText.Text = _messageText;
+    }
 
-        private void CancelButton_Click(object sender, RoutedEventArgs e)
-        {
-            this.DialogResult = false;
-            this.Close();
-        }
+    private void CancelButton_Click(object sender, RoutedEventArgs e)
+    {
+        this.DialogResult = false;
+        this.Close();
+    }
 
-        private void ConfirmButton_Click(object sender, RoutedEventArgs e)
-        {
-            this.DialogResult = true;
-            this.Close();
-        }
+    private void ConfirmButton_Click(object sender, RoutedEventArgs e)
+    {
+        this.DialogResult = true;
+        this.Close();
     }
 }
--- DocumentVersionControlSystem.UI/Popups/SelectSwitchOptionPopup.xaml.cs
+++ DocumentVersionControlSystem.UI/Popups/SelectSwitchOptionPopup.xaml.cs
@@ -1,36 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
+namespace DocumentVersionControlSystem.UI.Popups;
 
-namespace DocumentVersionControlSystem.UI.Popups
+
+public partial class SelectSwitchOptionPopup : Window
 {
-    /// <summary>
-    /// Interaction logic for SelectSwitchOptionPopup.xaml
-    /// </summary>
-    public partial class SelectSwitchOptionPopup : Window
-    {
-        InputPopup InputPopup;
-        public SelectSwitchOptionPopup()
-        {
-            InitializeComponent();
-        }
+    InputPopup InputPopup;
+    public SwitchOption switchOption { get; set; }
+    public string Description;
 
-        private void OkButton_Click(object sender, RoutedEventArgs e)
-        {
-            this.Close();
-            InputPopup = new InputPopup();
-            InputPopup.TitleText.Text = "Enter version description:";
-            InputPopup.ShowDialog();
-        }
+    public enum SwitchOption
+    {
+        DeleteNewer,
+        SaveAsTheLatest
+    }
+
+    public SelectSwitchOptionPopup(string versionDescription)
+    {
+        this.Description = versionDescription;
+        InitializeComponent();
+    }
+
+    private void OkButton_Click(object sender, RoutedEventArgs e)
+    {
+        this.DialogResult = true;
+        this.Close();
+        InputPopup = new InputPopup(this.Description);
+        InputPopup.TitleText.Text = "Enter version description:";
+        InputPopup.ShowDialog();
+
+        if (InputPopup.DialogResult == true)
+            Description = InputPopup.MessageText.Text;
+    }
+
+    private void DeleteNewer_Checked(object sender, RoutedEventArgs e)
+    {
+        switchOption = SwitchOption.DeleteNewer;
+    }
+
+    private void SaveAsTheLatest_Checked(object sender, RoutedEventArgs e)
+    {
+        switchOption = SwitchOption.SaveAsTheLatest;
     }
 }
